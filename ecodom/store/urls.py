68422cf845b51893ab17fed3f35fa61e57960705
maxl85 <maxl85@mail.ru>
2022-06-05 00:34:11 +0300

    Create template base.html

--- ecodom/store/urls.py
+++ ecodom/store/urls.py
@@ -2,6 +2,6 @@ from store import views
 from django.urls import path
 
 urlpatterns = [
-    path('', views.index, name='store'),
+    path('', views.index, name='home'),
     path('category/<int:category_id>', views.get_category, name='category'),
 ]
